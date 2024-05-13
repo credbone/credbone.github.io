d9c1077c4292e2a7624f2af1a3e743ea95073078
Ruben <sargsyanrubens@gmail.com>
2024-05-14 02:20:30 +0400

    hashrouter

--- creadbone/src/index.tsx
+++ creadbone/src/index.tsx
@@ -4,6 +4,8 @@ import "./configs/customTags";
 //import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter as Router } from "react-router-dom";
+
 
 import { isDesktop, isMobile } from "react-device-detect";
 
