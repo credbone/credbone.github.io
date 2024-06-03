b96c8dd42083695fad43c29c75ac988db81ae3ec
Ruben <sargsyanrubens@gmail.com>
2024-06-03 22:37:20 +0400

    ui fixes

--- credbone/src/template/components.tsx
+++ credbone/src/template/components.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Ripple from "../components/Ripple";
-import { IconHome } from "../components/icon/credIcons";
+import { IconHome, IconSearch } from "../components/icon/credIcons";
 import Scroll from "../components/scroll";
+
+
+import sampleImage from "../styles/images/samples/res_15.jpg";
+
+
 
 const Components: React.FC = () => {
   return (
@@ -15,12 +20,12 @@ const Components: React.FC = () => {
     >
 
       
-      <group data-direction="column" data-align="center" data-position="center" data-space-vertical="30" data-gap="30">
+      <group data-direction="column" data-align="center" data-position="center" data-space-vertical="40" >
       <group
           data-width="auto"
           
       >
-        <group data-direction="column"  data-gap="15" data-text-align="center">
+        <group data-direction="column"  data-gap="20" data-text-align="center" data-space-horizontal="30">
           <text
             data-weight="700"
             data-text-size="xxx-large"
@@ -32,7 +37,7 @@ const Components: React.FC = () => {
           </text>
           <text
             data-wrap="wrap"
-            data-length="400"
+            data-length="600"
             data-line="1.5"
             data-light=""
           >
@@ -44,35 +49,68 @@ const Components: React.FC = () => {
 
       <group data-scroll-mask="false" data-border="no" data-background="none"   data-width="auto">
       <Scroll>
-      <group data-gap="15" data-wrap="no" data-space-horizontal="30" >
+      <group  data-wrap="no" data-space="40" data-gap="10">
         
-        <Ripple>
-        <Link data-drag="none" data-type="group" data-ink-color="main-dark" data-contain="" data-interactive=""   to="../Colors"  data-background="main" data-color="main-text" data-length="200" data-height="300" data-radius="15">
-            <group data-index="1"data-direction="column" data-gap="15" data-space="30">
+       
+        <group data-radius="15" data-type="group" data-ink-color="main-dark" data-contain=""   data-background="main" data-color="main-text" data-length="300" data-height="300" >
+
+
+
+
+            <group data-index="1"data-direction="column"  data-gap="15"  data-space="30">
             <text data-text-size="x-large" data-weight="700" data-wrap="wrap">Color System</text>
           <text data-wrap="wrap" data-line="1.5">
           Flexible theme customization
               </text>
-              <IconHome size={50} />
+
+
+             {/* <Link data-type="group"  data-interactive=""  data-drag="none"  to="../Colors" data-position="bottom" >
+               <group  data-index="1"  data-background="main-text" data-width="auto" data-space="10" data-space-horizontal="20" data-radius="5" data-color="main" data-weight="700"> <text>More ...</text></group>
+               
+               </Link> */}
+
 </group>
-        </Link>
-      </Ripple>
+        </group>
+      
 
         
-        <group data-space="30" data-background="main-dark" data-color="white"  data-length="200" data-height="300" data-radius="15">
-          <text data-text-size="x-large" data-weight="700" data-wrap="wrap">Icons</text>
-          <text data-wrap="wrap" data-line="1.5">
+        <group data-radius="15" data-space="30" data-elevation="1" data-background="context"  data-length="350" data-height="300"  >
+<group data-direction="column" data-gap="15">
+<text data-text-size="x-large" data-weight="700" data-wrap="wrap">Icons</text>
+          <text data-wrap="wrap" data-line="1.5" data-max-length="200">
           Beautifully crafted and carefully designed icons.
           </text>
+</group>
+
         </group>
 
-        <group data-space="30" data-background="secondary" data-color="secondary-text"  data-length="200" data-height="300" data-radius="15">
-          <text data-text-size="x-large" data-weight="700" data-wrap="wrap">Buttons</text>
+        <group  data-radius="15" data-background="secondary" data-color="secondary-text"  data-length="240" data-height="300" >
+        {/* <picture data-position="absolute" data-name="color-demo">
+            <img src={sampleImage} alt="" />
+          </picture> */}
+
+
+          <group data-index="1" data-direction="column"  data-gap="15"  data-space="30">
+            <text data-text-size="x-large" data-weight="700" data-wrap="wrap">Buttons</text>
+          <text data-wrap="wrap" data-line="1.5">
+          Flexible theme customization
+              </text>
+
+
+</group>
+
         </group>
 
         
-        <group data-space="30" data-background="secondary-light" data-color="secondary-text"  data-length="200" data-height="300" data-radius="15">
-          <text data-text-size="x-large" data-weight="700" data-wrap="wrap">Checkbox & Switches</text>
+        <group data-radius="15" data-space="30" data-background="grey" data-color="white"  data-length="400" data-height="300">
+
+<group data-direction="column" data-gap="15">
+<text data-text-size="x-large" data-weight="700" data-wrap="wrap">Checkbox & Switches</text>
+          <text data-wrap="wrap" data-line="1.5" data-max-length="200">
+          Beautifully crafted and carefully designed icons.
+          </text>
+</group>
+
         </group>
 
       </group>
@@ -80,7 +118,7 @@ const Components: React.FC = () => {
       </group>
       </group>
 
-      <group data-height="90" data-shrink="0"></group>
+      <group data-height="90" data-shrink="no"></group>
       
     </view>
     
