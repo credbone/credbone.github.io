d4dd2580d343f091d33ba9c6774fa55a011613d2
ruben-sargsyan <sargsyanrubens@gmail.com>
2024-06-03 18:44:05 +0400

    ui fixes

--- credbone/src/template/components.tsx
+++ credbone/src/template/components.tsx
@@ -1,23 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Ripple from "../components/Ripple";
+import { IconHome } from "../components/icon/credIcons";
+import Scroll from "../components/scroll";
 
 const Components: React.FC = () => {
   return (
 
     <view
-      data-vertical=""
-      data-adaptive=""
-      data-space="30"
-      data-gap="15"
-      data-align="start"
+
+
+      data-scroll=""
+
     >
-      <view
-        data-size="medium"
-        data-height="auto"
-        data-max-height="fit"
-        data-radius="10"
-        data-elevation="1"
+
+      
+      <group data-direction="column" data-align="center" data-position="center" data-space-vertical="30" data-gap="30">
+      <group
+          data-width="auto"
+          
       >
-        <group data-direction="column" data-space="30" data-gap="15">
+        <group data-direction="column"  data-gap="15" data-text-align="center">
           <text
             data-weight="700"
             data-text-size="xxx-large"
@@ -29,7 +32,7 @@ const Components: React.FC = () => {
           </text>
           <text
             data-wrap="wrap"
-            data-length="300"
+            data-length="400"
             data-line="1.5"
             data-light=""
           >
@@ -37,7 +40,45 @@ const Components: React.FC = () => {
           </text>
         </group>
 
-      </view>
+      </group>
+
+      <group data-scroll-mask="false" data-border="no" data-background="none"   data-width="auto">
+      <Scroll>
+      <group data-gap="15" data-wrap="no" data-space-horizontal="30" >
+        
+        <Ripple>
+        <Link data-type="group" data-ink-color="main-dark" data-contain="" data-interactive=""   to="../Colors"  data-background="main" data-color="main-text" data-length="200" data-height="300" data-radius="15">
+            <group data-index="1"data-direction="column" data-gap="15" data-space="30">
+            <text data-text-size="x-large" data-weight="700" data-wrap="wrap">Color System</text>
+          <text data-wrap="wrap" data-line="1.5">
+          Flexible theme customization
+              </text>
+              <IconHome size={50} />
+</group>
+        </Link>
+      </Ripple>
+
+        
+        <group data-space="30" data-background="main-dark" data-color="white"  data-length="200" data-height="300" data-radius="15">
+          <text data-text-size="x-large" data-weight="700" data-wrap="wrap">Icons</text>
+        </group>
+
+        <group data-space="30" data-background="secondary" data-color="secondary-text"  data-length="200" data-height="300" data-radius="15">
+          <text data-text-size="x-large" data-weight="700" data-wrap="wrap">Buttons</text>
+        </group>
+
+        
+        <group data-space="30" data-background="secondary-light" data-color="secondary-text"  data-length="200" data-height="300" data-radius="15">
+          <text data-text-size="x-large" data-weight="700" data-wrap="wrap">Checkbox & Switches</text>
+        </group>
+
+      </group>
+</Scroll>
+      </group>
+      </group>
+
+      <group data-height="90" data-shrink="0"></group>
+      
     </view>
     
   );
