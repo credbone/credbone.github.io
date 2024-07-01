aacf3d0f36f52d0fe7ab587b5ac25a6d4a3bbedd
ruben-sargsyan <sargsyanrubens@gmail.com>
2024-07-01 17:12:37 +0400

    ui fixes

--- credbone/src/template/index.tsx
+++ credbone/src/template/index.tsx
@@ -16,7 +16,6 @@ import SubNavigation from "../components/subnav";
 import CheckboxSwitchers from "./CheckboxSwitchers";
 import InputsAndForms from "./InputsAndForms";
 
-
 import Popover from "../components/popover";
 import RichThemePicker from "./richThemePicker";
 import Ripple from "../components/Ripple";
@@ -25,6 +24,7 @@ import TooltipPopover from "./TooltipPopover";
 import Cards from "./Cards";
 import Miscellaneous from "./Miscellaneous";
 import { isDesktop } from "react-device-detect";
+import StuckReporter from "../components/StuckReporter";
 
 const Template: React.FC = () => {
   const location = useLocation();
@@ -73,47 +73,68 @@ const Template: React.FC = () => {
             <Route path="CardsAndList" element={<Cards />} />
             <Route path="Miscellaneous" element={<Miscellaneous />} />
           </Routes>
-          <group
-            data-index="3"
-           data-left="30"
-            data-bottom="30"
-            data-sticky="bottom"
-            data-width="auto"
-          >
-            <Popover
-              content={
-                <group
-                  data-animation-name="appear-bottom"
-                  data-fill-mode="backwards"
-                  data-animation-duration="1.25"
+          <StuckReporter>
+            {(isSticky) => (
+              <group
+                data-index="3"
+                data-left="30"
+                data-bottom="30"
+                data-sticky="bottom"
+                data-width="auto"
+                data-space-bottom="30"
+              >
+                <Popover
+                  content={
+                    <group
+                      data-animation-name="appear-bottom"
+                      data-fill-mode="backwards"
+                      data-animation-duration="1.25"
+                    >
+                      <RichThemePicker />
+                    </group>
+                  }
+                  data-space="5"
+                  data-radius="10"
+                  data-backdrop="10"
+                  data-width="auto"
                 >
-                  <RichThemePicker />
-                </group>
-              }
-              data-space="5"
-              data-radius="10"
-              data-backdrop="10"
-              data-width="auto"
-            >
-              <group data-width="auto">
-                <Ripple>
-                  <group
-                    data-contain=""
-                    data-length="60"
-                    data-height="60"
-                    data-radius="30"
-                    data-background="context"
-                    data-cursor="pointer"
-                    data-shrink="no"
-                    data-elevation="6"
-                    data-interactive=""
-                  >
-                    <icon data-position="center">opacity</icon>
+                  <group data-width="auto">
+                    <Ripple>
+                      <group
+                        data-contain=""
+                        data-width="auto"
+                        data-height={isSticky ? "60" : "50"}
+                        data-radius={isSticky ? "30" : "10"}
+                        data-background="context"
+                        data-cursor="pointer"
+                        data-shrink="no"
+                        data-elevation={isSticky ? "6" : "1"}
+                        data-interactive=""
+                        data-align="center"
+                        data-wrap="no"
+                        data-space={isSticky ? "0" : "20"}
+                        data-gap={isSticky ? "0" : "10"}
+                      >
+                        <group data-length={isSticky ? "60" : "20"}>
+                          <icon data-position="center">opacity</icon>
+                        </group>
+
+                        <text
+                          data-weight="600"
+                          data-duration=".225"
+                          data-opacity={isSticky ? "0" : ""}
+                          data-transition-prop="font-size"
+                          data-text-size={isSticky ? "0" : ""}
+                        >
+                          Change Theme
+                        </text>
+                      </group>
+                    </Ripple>
                   </group>
-                </Ripple>
+                </Popover>
               </group>
-            </Popover>
-          </group>
+            )}
+          </StuckReporter>
           <group data-height="100"></group>
         </group>
       </view>
