1c49f29ffc7bbdcac6f03e1b3003e7c540c6acad
Ruben <sargsyanrubens@gmail.com>
2025-01-22 21:34:41 +0400

    bottom nav fizes

--- credbone/src/pages/navigation/bottomNav.tsx
+++ credbone/src/pages/navigation/bottomNav.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { navItems } from "./subroutesData";
 import Tooltip from "../../components/tooltip";
-import {ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import Ripple from "../../components/Ripple";
 
 const BottomNav: React.FC = () => {
@@ -12,105 +12,101 @@ const BottomNav: React.FC = () => {
   );
 
   const prevItem = currentIndex > 0 ? navItems[currentIndex - 1] : null;
-  const nextItem = currentIndex < navItems.length - 1 ? navItems[currentIndex + 1] : null;
+  const nextItem =
+    currentIndex < navItems.length - 1 ? navItems[currentIndex + 1] : null;
 
   const isLastItem = currentIndex === navItems.length - 1;
 
   return (
+    <group data-space="30" data-gap="30">
+      <separator data-horizontal=""></separator>
 
+      <group data-wrap="no" data-gap="10" data-space-vertical="30">
+        {prevItem && (
+          <Ripple>
+            <Link
+              data-ink-color="neutral"
+              data-contain=""
+              data-interactive=""
+              data-over-color="neutral"
+              data-type="group"
+              data-width="auto"
+              // data-border="inset"
+              data-background="adaptive-gray"
+              data-space="20"
+              data-radius="15"
+              data-gap="20"
+              data-drag="none"
+              data-wrap="no"
+              to={`/${prevItem.to}`}
+            >
+              <group
+                data-height="fit"
+                data-align="center"
+                data-justify="center"
+                data-length="30"
+              >
+                <ChevronLeft />
+              </group>
 
-<group data-space="30" data-gap="30">
-<separator data-horizontal=""></separator>
+              {isLastItem && (
+                <group
+                  data-direction="column"
+                  data-width="auto"
+                  data-contain=""
+                >
+                  <text data-weight="700" data-ellipsis="">
+                    {prevItem.label}
+                  </text>
+                  <text data-opacity="30">Previous</text>
+                </group>
+              )}
+            </Link>
+          </Ripple>
+        )}
 
-<group data-wrap="no" data-gap="10" data-space-vertical="30">
-  {prevItem && (
-<Ripple>
-<Link
-        data-ink-color="neutral"
-        data-contain=""
-      data-interactive=""
-      data-over-color="neutral"
-      data-type="group"
-      data-width="auto"
-      data-border="inset"
-      data-space="20"
-      data-radius="15"
-      data-gap="20"
-      data-drag="none"
-      data-wrap="no"
-      to={`/${prevItem.to}`}
-    >
+        {nextItem && (
+          <Ripple>
+            <Link
+              data-min-length="200"
+              data-ink-color="neutral"
+              data-contain=""
+              data-background="adaptive-gray"
+              data-interactive=""
+              data-over-color="neutral"
+              data-type="group"
+              data-width="auto"
+              data-length="forcefit"
+              //   data-border=""
+              data-space="20"
+              data-radius="15"
+              data-gap="20"
+              data-drag="none"
+              data-wrap="no"
+              to={`/${nextItem.to}`}
+            >
+              <group data-direction="column" data-width="auto" data-contain="">
+                <text data-weight="700" data-ellipsis="">
+                  {nextItem.label}
+                </text>
+                <text data-opacity="30">Next</text>
+              </group>
 
-
-      <group
-        data-width="auto"
-        data-height="fit"
-        data-align="center"
-        data-justify="center"
-        data-radius="30"
-       
-      >
-        <ChevronLeft  />
+              <group
+                data-position="right"
+                data-height="fit"
+                data-align="center"
+                data-justify="center"
+                data-interact=""
+                data-length="30"
+              >
+                <ChevronRight />
+              </group>
+            </Link>
+          </Ripple>
+        )}
       </group>
-
-
-      {isLastItem && (
-               <group data-direction="column" data-width="auto" data-contain="">
-               <text data-opacity="30">Prevous</text>
-               <text data-weight="700" data-ellipsis="" >
-                 {prevItem.label}
-               </text>
-             </group>
-  )}
-
-
-
-    </Link>
-</Ripple>
-  )}
-
-  {nextItem && (
-    <Ripple>
-      <Link
-        data-ink-color="neutral"
-        data-contain=""
-        data-background="adaptive-gray"
-        data-interactive=""
-        data-over-color="neutral"
-        data-type="group"
-        data-width="auto"
-        data-length="forcefit"
-        //   data-border=""
-        data-space="20"
-        data-radius="15"
-        data-gap="20"
-        data-drag="none"
-        data-wrap="no"
-        to={`/${nextItem.to}`}
-      >
-        <group data-direction="column" data-width="auto" data-contain="">
-          <text data-opacity="30">Up Next</text>
-          <text data-weight="700" data-ellipsis="" >
-            {nextItem.label}
-          </text>
-        </group>
-
-        <group
-        data-width="auto"
-        data-position="right"
-            data-height="fit"
-            data-align="center"
-            data-justify="center"
-            data-interact=""
-          >
-            <ChevronRight />
-          </group>
-      </Link>
-    </Ripple>
-  )}
-</group>
-</group>
-
+    </group>
   );
 };
 
